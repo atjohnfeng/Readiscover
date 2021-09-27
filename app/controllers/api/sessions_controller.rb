286ce57456b32c71f41844ab9651bdabb8f8a098
atjohnfeng <atjohnfeng@gmail.com>
2021-09-26 23:34:25 -0700

    Add user and session controller

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,21 @@
+class Api::SessionsController < ApplicationController
+
+def create
+        @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+        if @user.nil?
+            render json: ['Incorrect username/password combination.'], status: 401
+        else
+            login!(@user)
+            render 'api/users/show'
+        end
+    end
+
+    def destroy
+        if logout!
+            return {}
+        else
+            render json: status: 404
+        end
+    end
+
+end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,50 @@
 class Api::UsersController < ApplicationController
+
+skip_before_action :verify_authenticity_token
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 401
+        end
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user && @user.update(user_params)
+            render :show
+        elsif !@user
+            render json: ['User could not be found.'], status: 400
+        else
+            render json: @user.errors.full_messages, status: 401
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        if @user
+            @user.destroy
+            render :show
+        else
+            render json: ['User could not be found.']
+        end
+    end
+
+    def index
+        @users = User.all
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :email)
+    end
+
 end
