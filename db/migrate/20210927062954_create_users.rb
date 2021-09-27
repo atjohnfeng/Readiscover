cbe3bee5b6ff915e4e867fb2568918affadac55f
atjohnfeng <atjohnfeng@gmail.com>
2021-09-27 09:25:48 -0700

    Add users model and migration

--- db/migrate/20210927062954_create_users.rb
+++ db/migrate/20210927062954_create_users.rb
@@ -1,9 +1,17 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      
+      t.string :username, null: false
+      t.string :email, null: false
+      t.string :profile_img_url, null: false
+      t.string :session_token, null: false
+      t.string :password_digest, null: false
 
       t.timestamps
     end
+
+    add_index :users, :session_token, unique: true
+    add_index :users, :username, unique: true
+    add_index :users, :email, unique: true
   end
 end
