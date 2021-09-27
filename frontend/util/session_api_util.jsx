11e181c791efd79239d36f9c7c0b77c5a906092b
atjohnfeng <atjohnfeng@gmail.com>
2021-09-27 11:20:59 -0700

    Add session api util

---
+++ frontend/util/session_api_util.jsx
@@ -0,0 +1,22 @@
+export const login = user => (
+    $.ajax({
+        url: '/api/session',
+        method: 'post',
+        data: { user }
+    })
+)
+
+export const logout = () => (
+    $.ajax({
+        url: '/api/session',
+        method: 'delete'
+    })
+)
+
+export const signup = user => (
+    $.ajax({
+        url: '/api/users',
+        method: 'post',
+        data: { user }
+    })
+)
