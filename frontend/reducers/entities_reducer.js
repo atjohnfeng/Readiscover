06891754d0f165edb995cb116de0936d9fb03df2
atjohnfeng <atjohnfeng@gmail.com>
2021-09-27 12:07:21 -0700

    oops

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
+import errorsReducer from './errors_reducer';
+import sessionReducer from './session_reducer';
 
 const entitiesReducer = combineReducers({
-    users: usersReducer
+    users: usersReducer,
+    errors: errorsReducer,
+    session: sessionReducer,
 });
 
 export default entitiesReducer;
