8d22a5e541070ac85f932022502b15010844e52e
atjohnfeng <atjohnfeng@gmail.com>
2021-09-27 12:08:04 -0700

    Added additional reducers to root

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,12 +1,8 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
-import errorsReducer from './errors_reducer';
-import sessionReducer from './session_reducer';
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
-    errors: errorsReducer,
-    session: sessionReducer,
 });
 
 export default entitiesReducer;
