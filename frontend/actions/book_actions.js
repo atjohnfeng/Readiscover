c9c56f3266ed55f0b7413dd5e190ea925ccf8d28
atjohnfeng <atjohnfeng@gmail.com>
2021-09-29 23:01:39 -0700

    Add book actions

---
+++ frontend/actions/book_actions.js
@@ -0,0 +1,24 @@
+import * as BookApiUtil from '../util/book_api_util';
+
+export const RECEIVE_BOOK = 'RECEIVE_BOOK';
+export const RECEIVE_BOOKS = 'RECEIVE_BOOKS';
+
+const receiveBook = book => ({
+    type: RECEIVE_BOOK,
+    book
+})
+
+const receiveBooks = books => ({
+    type: RECEIVE_BOOKS,
+    books
+})
+
+export const getBook = book => dispatch => (
+    BookApiUtil.getBook(book)
+        .then(book => dispatch(receiveBook(book)))
+)
+
+export const getBooks = () => dispatch (
+    BookApiUtil.getBooks()
+        .then(books => dispatch(receiveBooks(books)))
+)
