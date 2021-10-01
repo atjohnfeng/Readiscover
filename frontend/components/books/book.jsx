4410a5a435943497ab788de2ff8902a8fc8fdfed
atjohnfeng <atjohnfeng@gmail.com>
2021-10-01 11:04:09 -0700

    Add additional seeds

--- frontend/components/books/book.jsx
+++ frontend/components/books/book.jsx
@@ -12,13 +12,17 @@ class Book extends React.Component {
     }
 
     render() {
+        if (!this.props.book) {
+            return null;
+        }
+
         return (
             <div className="book-show">
                 <NavbarContainer />
                 <div className="book-show-container">
-                    {/* <img className="book-show-book-cover"
+                    {<img className="book-show-book-cover"
                         src={this.props.book.cover_img_url}
-                        alt={this.props.book.cover_img_url} /> */
+                        alt={this.props.book.cover_img_url} /> 
                         // console.log(this.props.book)
                         }
                 </div>
