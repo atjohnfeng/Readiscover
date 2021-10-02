fbab9c84f39d0e010c36d7138d040bbb4cc49556
atjohnfeng <atjohnfeng@gmail.com>
2021-10-01 19:17:50 -0700

    Add 404 page content

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavbarContainer from '../navbar/navbar_container';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
     
@@ -10,18 +11,17 @@ class Home extends React.Component {
     render() {
         return (
             <div>
-                <div className="header-div"></div>
+                <div className="header-div"><NavbarContainer /></div>
                     <div className="home">
-                        <div className="nav-bar-container">
-                            <NavbarContainer />
-                            <div className="home-content-container">
-                                Welcome to your home page, 
-                                {this.props.currentUser.username}!
-                                <br />
-                                Visit 'All Books' to view all books in our database.
-                                <br />
-                                Under Construction.
-                            </div>
+                        <div className="home-content-container">
+                            <br />
+                            Welcome to your home page, {this.props.currentUser.username}!
+                            <br />
+                            Visit 'All Books' to view all books in our database.
+                            <br />
+                            Under Construction.
+                            <br />
+                            <Link to="/notfound">Take a trip to the end of the sidewalk.</Link>
                         </div>
                     </div>
                 <div className="footer-div"></div>
--- frontend/components/home/not_found.jsx
+++ frontend/components/home/not_found.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavbarContainer from '../navbar/navbar_container';
-
+import { Link } from 'react-router-dom';
 
 class NotFound extends React.Component {
 
@@ -10,9 +10,39 @@ class NotFound extends React.Component {
                 <div className="header-div">
                     <NavbarContainer />
                 </div>
-                    <div className="book-index">
-                        <div className="404-not-found">
-                            <h1>404: Page Not Found</h1>
+                    <div className="not-found">
+                        <div className="nf-poem">
+                            <div className="nf-header">
+                                Sorry, you've reached the end of the sidewalk.
+                            </div>
+                            <br /><br />
+                            <div className="nf-quote">
+                                Yes we'll walk with a walk that is measured and slow,
+                                <br />
+                                And we'll go where the chalk-white arrows go,
+                                <br />
+                                For the children, they mark, and the children, they know
+                                <br />
+                                The place where the sidewalk ends.
+                            </div>
+                            <br /><br />
+                            <div className="nf-author">
+                                -- Shel Silverstein, <i>Where the Sidewalk Ends</i>
+                            </div>
+                            <br /><br />
+                            <button>
+                                <Link className="nf-button"
+                                    to="/home">Back to the Readiscover homepage
+                                </Link>
+                            </button>
+                        </div>
+                        <div className="nf-images">
+                            <img className="shel"
+                                src="https://s.gr-assets.com/assets/error_pages/not_found/sidewalk_author-3fea6f0f59ee4acb7b4ec42a1a9b2996.png"
+                                alt="shel" />
+                            <img className="giving-tree"
+                                src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1168052448l/30119._SX318_.jpg"
+                                alt="giving-tree-cover" />
                         </div>
                     </div>
                 <div className="footer-div"></div>
