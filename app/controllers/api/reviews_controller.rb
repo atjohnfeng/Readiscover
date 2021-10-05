65705e7749bddaa92089c26dbfec9a7a48ffb56d
atjohnfeng <atjohnfeng@gmail.com>
2021-10-05 11:44:27 -0700

    Add outline and jbuilder files

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,2 +1,23 @@
 class Api::ReviewsController < ApplicationController
+
+    def index
+
+    end
+
+    def show
+        
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
 end
