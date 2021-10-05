9e6046d89435c7235453df831e88848adf9d902b
atjohnfeng <atjohnfeng@gmail.com>
2021-10-05 11:13:32 -0700

    Add review conditional validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,16 @@
 class Review < ApplicationRecord
 
     validates :user_id, :book_id, presence: true
+    validates :rating, presence: true, unless: :body_present?
+    validates :body, presence: true, unless: :rating_present?
+
+    def body_present?
+        validates :body, presence: true
+    end
+
+    def rating_present?
+        validates :rating, presence: true
+    end
 
     belongs_to :user,
     foreign_key: :user_id,
