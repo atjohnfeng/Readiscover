d3b1a868159a47b57b5b3e861fc25aa4750a641d
atjohnfeng <atjohnfeng@gmail.com>
2021-10-05 11:02:16 -0700

    Add basic review validations and associations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,13 @@
 class Review < ApplicationRecord
+
+    validates :user_id, :book_id, presence: true
+
+    belongs_to :user,
+    foreign_key: :user_id,
+    class_name: :User
+
+    belongs_to :book,
+    foreign_key: :book_id,
+    class_name: :Book
+
 end
