f95eff28a1a4dbc2045e6cd5913ac975c834ce7b
atjohnfeng <atjohnfeng@gmail.com>
2021-10-05 11:16:34 -0700

    Add reviews routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     resources :users, only:[:create]
-    resources :books, only:[:show, :index]
-
+    resources :books, only:[:show, :index] do
+      resources :reviews, only:[:show, :index, :create, :update, :destroy]
+    end
     resource :session, only:[:create, :destroy]
   end
 
