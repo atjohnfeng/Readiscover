46634a86c502f9ab9b323933c0f14ec897439b84
atjohnfeng <atjohnfeng@gmail.com>
2021-10-05 23:07:35 -0700

    Add review actions

---
+++ frontend/actions/review_actions.js
@@ -0,0 +1,54 @@
+import * as ReviewApiUtil from '../util/review_api_util';
+
+export const RECEIVE_REVIEW = 'RECEIVE_REVIEW';
+export const RECEIVE_REVIEWS = 'RECEIVE_REVIEWS';
+export const RECEIVE_REVIEW_ERRORS = 'RECEIVE_REVIEW_ERRORS';
+export const DELETE_REVIEW = 'DELETE_REVIEW';
+
+const receiveReview = review => {
+    return {
+        type: RECEIVE_REVIEW,
+        review
+    }
+}
+
+const receiveReviews = reviews => {
+    return {
+        type: RECEIVE_REVIEWS,
+        reviews
+    }
+}
+
+const receiveReviewErrors = errors => {
+    return {
+        type: RECEIVE_REVIEW_ERRORS,
+        errors
+    }
+}
+
+const deleteReview = review => {
+    return {
+        type: DELETE_REVIEW,
+        review
+    }
+}
+
+export const getReviews = bookId => dispatch => (
+    ReviewApiUtil.getReviews(bookId).then(reviews => dispatch(receiveReviews(reviews)),
+        errors => dispatch(receiveReviewErrors(errors.responseJSON)))
+)
+
+export const getReview = (bookId, id) => dispatch => (
+    ReviewApiUtil.getReview(bookId, id).then(review => dispatch(receiveReview(review)),
+        errors => dispatch(receiveReviewErrors(errors.responseJSON)))
+)
+
+export const editReview = review => dispatch => (
+    ReviewApiUtil.editReview(review).then(review => dispatch(receiveReview(review)),
+        errors => dispatch(receiveReviewErrors(errors.responseJSON)))
+)
+
+export const deleteReview = review => dispatch => (
+    ReviewApiUtil.deleteReview(review).then(review => dispatch(deleteReview(review)),
+        errors => dispatch(receiveReviewErrors(errors.responseJSON)))
+)
