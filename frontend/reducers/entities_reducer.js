2ca0e08f0d22403f9967d77e232771a4e25d0885
atjohnfeng <atjohnfeng@gmail.com>
2021-10-05 23:22:27 -0700

    add reviews reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import booksReducer from './books_reducer';
+import reviewsReducer from './reviews_reducer';
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
-    books: booksReducer
+    books: booksReducer,
+    reviews: reviewsReducer
 });
 
 export default entitiesReducer;
---
+++ frontend/reducers/reviews_reducer.js
@@ -0,0 +1,22 @@
+import { RECEIVE_REVIEW, 
+    RECEIVE_REVIEWS, 
+    DELETE_REVIEW } from '../actions/book_actions'
+
+const reviewsReducer = (state = {}, action) => {
+    Object.freeze(state);
+
+    switch (action.type) {
+        case RECEIVE_REVIEWS:
+            return action.reviews;
+        case RECEIVE_REVIEW:
+            return Object.assign({}, state, action.review);
+        case DELETE_REVIEW:
+            let nextState = Object.assign({}, state);
+            delete nextState[action.review.id];
+            return nextState;
+        default:
+            return state;
+    }
+};
+
+export default reviewsReducer;
