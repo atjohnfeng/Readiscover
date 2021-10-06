c12e1d7ad97acc330e7d910a31915d6bf5630ed2
atjohnfeng <atjohnfeng@gmail.com>
2021-10-05 23:11:02 -0700

    Add review errors reducer

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import sessionErrorsReducer from "./session_errors_reducer";
+import reviewErrorsReducer from "./reviews_errors_reducer";
 
 const errorsReducer = combineReducers({
     sessionErrors: sessionErrorsReducer,
+    reviewErrors: reviewErrorsReducer
 });
 
 export default errorsReducer;
