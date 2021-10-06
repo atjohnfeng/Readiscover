9f425cab0206e14d14ecb85a4d7c05c2fdf31997
atjohnfeng <atjohnfeng@gmail.com>
2021-10-06 11:13:16 -0700

    additional star rating code

--- frontend/components/reviews/star_form.jsx
+++ frontend/components/reviews/star_form.jsx
@@ -4,26 +4,65 @@ class StarForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            rating: null
+            review: null,
+            rating: null,
+            bookId: this.props.bookId,
+            userId: this.props.currentUser
         }
 
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentDidMount() {
-        this.props.getReview(this.props.bookId, this.props.currentUser);
+        const review = this.props.getReview(this.props.bookId, this.props.currentUser)
+        if (!!review) {
+            this.setState({
+                review: review,
+                rating: review.rating
+            })
+        }
     }
 
-    update() {
-
+    update(value) {
+        return e => {
+            this.setState({
+                rating: value
+            });
+            this.handleSubmit();
+        }
     }
 
-    handleSubmit() {
-
+    handleSubmit(e) {
+        e.preventDefault();
+        let rating;
+        if (!this.state.review) {
+            rating = Object.assign({}, this.state.rating, this.state.userId, this.state.bookId)
+        }
+        this.props.createReview(rating);
     }
 
     render() {
-        
+        return (
+            <div className="star-rating-form-container">
+                <form className="star-rating-form">
+                    <input type="radio" onClick={this.update(1)}
+                        value="1" className="rating-button">
+                    </input>
+                    <input type="radio" onClick={this.update(2)}
+                        value="2" className="rating-button">
+                    </input>
+                    <input type="radio" onClick={this.update(3)}
+                        value="3" className="rating-button">
+                    </input>
+                    <input type="radio" onClick={this.update(4)}
+                        value="4" className="rating-button">
+                    </input>
+                    <input type="radio" onClick={this.update(5)}
+                        value="5" className="rating-button">
+                    </input>
+                </form>
+            </div>
+        )
     }
 
 }
