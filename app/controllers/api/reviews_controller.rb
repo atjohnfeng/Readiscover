cb9014324595c7522387a8c887541d2602d5b2f8
atjohnfeng <atjohnfeng@gmail.com>
2021-10-05 20:48:48 -0700

    add reviews controller?

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,23 +1,52 @@
 class Api::ReviewsController < ApplicationController
 
     def index
-
+        @reviews = Review.where(book_id: params[:book_id])
+        render :index
     end
 
     def show
-        
+        if !!current_user
+            @review = Review.find_by(book_id: params[:book_id], user_id: current_user.id)
+            render :show
+        end
     end
 
     def create
-
+        @review = Review.new(review_params)
+        if @review.save
+            render :show
+        else
+            render json: @review.errors.full_messages, status: 422
+        end
     end
 
     def update
-
+        if !!current_user
+            @review = Review.find_by(book_id: params[:book_id], user_id: current_user.id)
+            if @review.update(review_params)
+                render :show
+            else
+                render json: @review.errors.full_messages, status: 422
+            end
+        end
     end
 
     def destroy
+        if !!current_user
+            @review = Review.find_by(book_id: params[:book_id], user_id: current_user.id)
+            if @review.destroy
+                render :show
+            else
+                render json: ['Delete failed.'], status: 422
+            end
+        end
+    end
 
+    private
+
+    def review_params
+        params.require(:review).permit(:title, :body, :rating)
     end
 
 end
