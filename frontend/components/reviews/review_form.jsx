9ce1d378cc1809137e80d64d946b3a5b4bed174d
John Feng <atjohnfeng@gmail.com>
2021-10-29 17:56:13 -0700

    Add simple mobile styling for 404 page

--- frontend/components/reviews/review_form.jsx
+++ frontend/components/reviews/review_form.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import NavbarContainer from '../navbar/navbar_container';
 
 class ReviewForm extends React.Component {
