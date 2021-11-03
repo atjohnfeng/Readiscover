488927476c60fceff10552d1004c059c5d8ac71d
John Feng <atjohnfeng@gmail.com>
2021-11-02 17:43:13 -0700

    Add bookshelve controller

--- app/controllers/api/bookshelves_controller.rb
+++ app/controllers/api/bookshelves_controller.rb
@@ -11,20 +11,47 @@ class BookshelvesController < ApplicationController
     end
 
     def create
-
+        @book_to_shelve = Bookshelf.new(book_to_shelve_params)
+        if @book_to_shelve.user_id != current_user.id
+            render json: ['Please log into the correct account you are trying to shelve a book for.'], status: 422
+            return
+        end
+        if @book_to_shelve.save
+            render :show
+        else
+            render json: @book_to_shelve.errors.full_messages, status: 422
+        end
     end
 
     def update
-
+        @book_to_shelve = Bookshelf.find_by(id: params[:id])
+        if @book_to_shelve.user_id != current_user.id
+            render json: ['You are not logged in the correct account.'], status: 422
+            return
+        end
+        if @book_to_shelve.update(book_to_shelve_params)
+            render :show
+        else
+            render json: @book_to_shelve.errors.full_messages, status: 422
+        end
     end
 
     def destroy
-
+        @book_to_shelve = Bookshelf.find_by(id: params[:id])
+        if @book_to_shelve.user_id != current_user.id
+            render json: ['You do not own this shelving.'], status: 422
+            return
+        end
+        if @book_to_shelve.destroy
+            render :show
+        else
+            render json: ['Failed to delete shelving.'], status: 422
+        end
     end
 
     private
 
-    def shelve_book_params
+    def book_to_shelve_params
         params.require(:bookshelf).permit(:user_id, :book_id, :shelf)
     end
 
