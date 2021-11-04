dccc30e0c058d44383e27830ee121f8b74dbb6ca
John Feng <atjohnfeng@gmail.com>
2021-11-04 13:45:03 -0700

    Complete bookshelves reducer

--- frontend/reducers/bookshelves_reducer.js
+++ frontend/reducers/bookshelves_reducer.js
@@ -1,23 +1,26 @@
 import {
-    
+    RECEIVE_SHELVING,
+    RECEIVE_BOOKSHELF,
+    DELETE_SHELVING,
+    RESET_SHELVING
 } from '../actions/bookshelf_actions'
 
 const bookshelvesReducer = (state = {}, action) => {
     Object.freeze(state);
 
     switch (action.type) {
-        // case RECEIVE_REVIEWS:
-        //     return action.reviews;
-        // case RECEIVE_REVIEW:
-        //     return Object.assign({}, state, action.review);
-        // case DELETE_REVIEW:
-        //     let nextState = Object.assign({}, state);
-        //     delete nextState[action.review.id];
-        //     return nextState;
-        // case RESET_REVIEWS:
-        //     return {};
-        // default:
-        //     return state;
+        case RECEIVE_SHELVING:
+            return action.shelving;
+        case RECEIVE_BOOKSHELF:
+            return Object.assign({}, state, action.bookshelf);
+        case DELETE_SHELVING:
+            let nextState = Object.assign({}, state);
+            delete nextState[action.shelving.id];
+            return nextState;
+        case RESET_SHELVING:
+            return {};
+        default:
+            return state;
     }
 };
 
