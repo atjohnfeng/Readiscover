f861b9fa3869f6d4431aa0ff67241e6b039b2236
John Feng <atjohnfeng@gmail.com>
2021-11-04 20:25:22 -0700

    fix backend controller

--- app/controllers/api/bookshelves_controller.rb
+++ app/controllers/api/bookshelves_controller.rb
@@ -11,28 +11,28 @@ class Api::BookshelvesController < ApplicationController
     end
 
     def create
-        @book_to_shelve = Bookshelf.new(book_to_shelve_params)
-        if @book_to_shelve.user_id != current_user.id
+        @shelving = Bookshelf.new(book_to_shelve_params)
+        if @shelving.user_id != current_user.id
             render json: ['Please log into the correct account you are trying to shelve a book for.'], status: 422
             return
         end
-        if @book_to_shelve.save
+        if @shelving.save
             render :show
         else
-            render json: @book_to_shelve.errors.full_messages, status: 422
+            render json: @shelving.errors.full_messages, status: 422
         end
     end
 
     def update
-        @book_to_shelve = Bookshelf.find_by(id: params[:id])
-        if @book_to_shelve.user_id != current_user.id
+        @shelving = Bookshelf.find_by(id: params[:id])
+        if @shelving.user_id != current_user.id
             render json: ['You are not logged in the correct account.'], status: 422
             return
         end
-        if @book_to_shelve.update(book_to_shelve_params)
+        if @shelving.update(book_to_shelve_params)
             render :show
         else
-            render json: @book_to_shelve.errors.full_messages, status: 422
+            render json: @shelving.errors.full_messages, status: 422
         end
     end
 
